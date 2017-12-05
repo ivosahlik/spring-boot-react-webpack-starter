37240bc2266ec2b789518fe0c99ca59b20f14a2f
Ivo Vošahlík <ivosahlik@seznam.cz>
2017-12-05 23:04:42 +0100

    delete skeleton, add webpack.config.js a set path, bundle.js, add .babelrc

---
+++ src/main/frontend/src/app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../../style/style.css'
+
+
+
+ReactDOM.render(
+    <h1>Hello</h1>,
+    document.getElementById('root')
+)
